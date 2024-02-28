82e3535aeb79f64d3e6a969065a4777c1cf99b42
horsicq <horsicq@gmail.com>
2024-02-28 19:08:39 +0100

    Update file(s): 2024-02-28

--- xshortcuts.cmake
+++ xshortcuts.cmake
@@ -2,9 +2,14 @@ include_directories(${CMAKE_CURRENT_LIST_DIR})
 
 set(XSHORTCUTS_SOURCES
     ${CMAKE_CURRENT_LIST_DIR}/dialogshortcuts.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogshortcuts.h
     ${CMAKE_CURRENT_LIST_DIR}/dialogshortcuts.ui
     ${CMAKE_CURRENT_LIST_DIR}/xshortcuts.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xshortcuts.h
     ${CMAKE_CURRENT_LIST_DIR}/xshortcutsdialog.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xshortcutsdialog.h
     ${CMAKE_CURRENT_LIST_DIR}/xshortcutstscrollarea.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xshortcutstscrollarea.h
     ${CMAKE_CURRENT_LIST_DIR}/xshortcutswidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xshortcutswidget.h
     )
