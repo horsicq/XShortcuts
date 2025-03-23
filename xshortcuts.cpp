30d1da4436e127bcfdd7a3cbaaf476a1c3524316
horsicq <horsicq@gmail.com>
2025-03-23 01:06:11 +0100

    Update file(s): 2025-03-23

--- xshortcuts.cpp
+++ xshortcuts.cpp
@@ -1347,7 +1347,8 @@ void XShortcuts::_addMenuItem(QList<MENUITEM> *pListMenuItems, quint64 nShortcut
     pListMenuItems->append(record);
 }
 
-void XShortcuts::_addMenuItem_Text(QList<MENUITEM> *pListMenuItems, quint64 nShortcutId, const QObject *pRecv, const char *pMethod, quint64 nSubgroups, const QString &sText)
+void XShortcuts::_addMenuItem_Text(QList<MENUITEM> *pListMenuItems, quint64 nShortcutId, const QObject *pRecv, const char *pMethod, quint64 nSubgroups,
+                                   const QString &sText)
 {
     MENUITEM record = {};
 
